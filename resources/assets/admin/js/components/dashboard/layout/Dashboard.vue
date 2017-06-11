62adf9c9561f1d39759d4afb57ca0f02a75e3be7
Artik <artem-makovetskiy@mail.ru>
2017-06-11 11:27:14 +0300

    added Dashboard.vue component

---
+++ resources/assets/admin/js/components/dashboard/layout/Dashboard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <main-header></main-header>
+
+        <main-sidebar v-if="userLoggedIn" ></main-sidebar>
+
+        <!-- =============================================== -->
+
+        <content-wrapper>
+            <div slot="header"><slot name="header"></slot></div>
+            <div slot="content"><slot name="content"></slot></div>
+        </content-wrapper>
+
+        <!-- =============================================== -->
+
+        <main-footer></main-footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [],
+        computed: {
+            userLoggedIn() {
+                return this.$store.state.userLoggedIn;
+            }
+        },
+        //components: ['main-header', 'main-sidebar', 'content-wrapper', 'main-footer'],
+        mounted() {
+            console.log('Component Dashboard mounted.')
+        }
+    }
+</script>
+
+<style>
+
+</style>
